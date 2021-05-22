0514a43a8c7bc633a96a3ec8cdcc3fbbbaafdbb7
kchensu <kchensu@sfu.ca>
2021-05-22 11:32:06 -0700

    finished header(center icons)

--- components/Header.js
+++ components/Header.js
@@ -12,13 +12,13 @@ import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       <div className="flex items-center">
         <Image src="/facebook.png" width={90} height={50} layout="fixed" />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-500" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-400"
+            className="flex-shrink flex ml-2 items-center bg-transparent outline-none placeholder-gray-400"
             type="text"
             placeholder="Search Fakebook"
           />
@@ -27,7 +27,7 @@ function Header() {
 
       {/*center*/}
       <div className="flex justify-center flex-grow">
-        <div className="flex">
+        <div className="flex space-x-3 md:space-x-2">
           <HeaderItem Icon={HomeIcon} />
           <HeaderItem Icon={FilmIcon} />
           <HeaderItem Icon={ShoppingCartIcon} />
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,7 +1,14 @@
 function HeaderItem({ Icon }) {
   return (
-    <div>
-      <Icon className="h-6" />
+    <div
+      className="cursor-pointer md:px-10 sm:h-14
+    md:hover:bg-gray-100 flex items-center rounded-xl 
+    active:border-b-2 active:border-blue-500 group"
+    >
+      <Icon
+        className="h-6 group-hover:text-blue-500
+      text-center sm:h-7 mx-auto"
+      />
     </div>
   );
 }
